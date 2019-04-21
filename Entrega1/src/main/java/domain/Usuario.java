fa697d43e51ab7ed5d1cb4cdb564e56437b8a9e5
MauroCicerchia <maurocicerchia98@gmail.com>
2019-04-21 17:59:59 -0300

    Fixed Usuario y TestUsuario

--- Entrega1/src/main/java/domain/Usuario.java
+++ Entrega1/src/main/java/domain/Usuario.java
@@ -2,14 +2,18 @@ package domain;
 import java.util.*;
 
 public class Usuario {
-	private HashSet<Guardarropa> = new HashSet<Guardarropa>() ;
+	
+	HashSet<Guardarropa> guardarropas = new HashSet<Guardarropa>() ;
+	
 	public void agregarGuardarropa(Guardarropa nuevoGuardarropa) {
-		this.guardarropa.add(nuevoGuardarropa);
+		this.guardarropas.add(nuevoGuardarropa);
 	}
+	
 	public void eliminarGuardarropa(Guardarropa guardarropa) {
-		this.guardarropa.remove(guardarropa);
+		this.guardarropas.remove(guardarropa);
 	}
-	public Guardarropa getGuardarropa() {
-		return this.guardarropas;
+
+	public Boolean tieneGuardarropa(Guardarropa g) {
+		return guardarropas.contains(g);
 	}
 }
