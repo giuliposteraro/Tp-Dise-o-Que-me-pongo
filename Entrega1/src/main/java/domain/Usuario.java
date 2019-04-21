066d03b69deaa4f8e99bb8b7be2f42d98f84fa8b
Giuliana <giuliposteraro@hotmail.com>
2019-04-21 17:17:03 -0300

    Clase usuario terminada

--- Entrega1/src/main/java/domain/Usuario.java
+++ Entrega1/src/main/java/domain/Usuario.java
@@ -1,5 +1,15 @@
 package domain;
+import java.util.*;
 
 public class Usuario {
-
+	private HashSet<Guardarropa> = new HashSet<Guardarropa>() ;
+	public void agregarGuardarropa(nuevoGuardarropa) {
+		this.guardarropa.add(nuevoGuardarropa);
+	}
+	public void eliminarGuardarropa(guardarropa) {
+		this.guardarropa.remove(guardarropa);
+	}
+	public Guardarropa getGuardarropas() {
+		return this.guardarropas;
+	}
 }
