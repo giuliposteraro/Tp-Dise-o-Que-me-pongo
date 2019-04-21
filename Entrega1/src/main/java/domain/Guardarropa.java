91cd3740d2fededdd04ca00925ded058bc57b908
nvent97 <37757410+nvent97@users.noreply.github.com>
2019-04-21 20:46:57 -0300

    Correccion sugerencias Guardaropa

--- Entrega1/src/main/java/domain/Guardarropa.java
+++ Entrega1/src/main/java/domain/Guardarropa.java
@@ -3,6 +3,7 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
@@ -11,26 +12,35 @@ import com.google.common.collect.Sets;
 public class Guardarropa {
 	
 	ArrayList<Prenda> prendas;
-	ArrayList<Atuendo> sugerencias;
 	
 	ArrayList<Prenda> prendasSuperiores;
 	ArrayList<Prenda> prendasInferiores;
 	ArrayList<Prenda> calzados;
 	ArrayList<Prenda> accesorios;
 	
-	public ArrayList<Atuendo> generarSugerencias() {
-		Set<List<ArrayList<Prenda>>> prendasSueltas = sugerenciasDePrendas();
+	public List<Atuendo> generarSugerencias() {
+		Set<List<Prenda>> prendasSueltas = sugerenciasDePrendas();
 		return crearAtuendos(prendasSueltas);
 	}
 	
-	public Set<List<ArrayList<Prenda>>> sugerenciasDePrendas(){
-		return Sets.cartesianProduct(ImmutableList.of(ImmutableSet.of(prendasSuperiores),ImmutableSet.of(prendasInferiores),ImmutableSet.of(calzados),ImmutableSet.of(accesorios)));
+	public Boolean tienePrenda (Prenda unaPrenda) {
+		return prendas.contains(unaPrenda);
 	}
 	
-	public ArrayList<Atuendo> crearAtuendos(Set<List<ArrayList<Prenda>>> prendasSueltas){
-		return prendasSueltas.stream().forEach(conjuntoDePrendas -> new Atuendo(conjuntoDePrendas));
+	private Set<List<Prenda>> sugerenciasDePrendas(){
+		return Sets.cartesianProduct(ImmutableList.of(ImmutableSet.copyOf(prendasSuperiores),ImmutableSet.copyOf(prendasInferiores),ImmutableSet.copyOf(calzados),ImmutableSet.copyOf(accesorios)));
 	}
 	
-
+	private List<Atuendo> crearAtuendos(Set<List<Prenda>> prendasSueltas){
+		return prendasSueltas.stream().map(conjuntoDePrendas -> new Atuendo(conjuntoDePrendas)).collect(Collectors.toList());
+	}
+	
+	private void actualizarCategorias() {
+		prendasSuperiores = prendas.stream().filter(p->p.getGategoria()==ECategoria.SUPERIOR);
+		prendasInferiores = prendas.stream().filter(p->p.getGategoria()==ECategoria.INFERIOR);
+		calzados = prendas.stream().filter(p->p.getGategoria()==ECategoria.CALZADO);
+		accesorios = prendas.stream().filter(p->p.getGategoria()==ECategoria.ACCESORIO);
+	}
+	
 	
 }
