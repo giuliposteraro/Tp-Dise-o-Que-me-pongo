3abd56834e1b1381d0c998f92f8150fbff4c2814
nvent97 <37757410+nvent97@users.noreply.github.com>
2019-10-22 21:28:36 -0300

    Correccion front end registro

--- TPA/src/main/java/server/controllers/LoginController.java
+++ TPA/src/main/java/server/controllers/LoginController.java
@@ -25,7 +25,7 @@ public class LoginController extends Controller {
 			}
 		}
 
-		return this.render("login.hbs");
+		return this.render("register.hbs");
 	}
 
 	public String logout(Request req, Response res) {
