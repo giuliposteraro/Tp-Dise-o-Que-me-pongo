766e2f3f1af77503cc5875490f6b6b7688a4614a
MauroCicerchia <maurocicerchia98@gmail.com>
2019-11-27 21:12:19 -0300

    vuelven los famosos jobs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@
 import org.uqbar.arena.Application;
 import org.uqbar.arena.windows.Window;
 
+import domain.Job;
+import domain.clima.TareaAlertasMeteorologicas;
+import domain.eventos.TareaSugerenciaEventos;
 import scenes.ListadoEventos.ListadoEventosView;
 import server.Router;
 import spark.Spark;
@@ -10,9 +13,8 @@ import spark.debug.DebugScreen;
 
 public class Main extends Application {
 	public static void main(String[] args) {
-//		new Job(new TareaSugerenciaEventos(), 5000, 10000).ejecutar();
-//		new Job(new TareaAlertasMeteorologicas(), 10000, 100000).ejecutar();
-//		new Main().start();
+		new Job(new TareaSugerenciaEventos(), 5000, 10000).ejecutar();
+		new Job(new TareaAlertasMeteorologicas(), 10000, 100000).ejecutar();
 		Spark.ipAddress("0.0.0.0");
 		Spark.port(Integer.parseInt(System.getenv("PORT")));		
 		Spark.staticFileLocation("/public");
